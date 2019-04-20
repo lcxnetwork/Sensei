fbb9d770ba0547e0705dffb76cb14e36a0239523
ExtraHash <ExtraHash@protonmail.com>
2019-04-20 01:35:15 -0600

    storing the nodes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -10,18 +10,41 @@ const db = require('../utils/utils').knex;
 
 // Preference Panel
 router.get('/', permission(), async function(req, res, next) {
+  const nodeList = await db('users')
+  .select('nodes')
+  .from('users')
+  .where('id', req.user.id)
+  .limit(1);
   res.render('dashboard', {
     title: 'Dashboard',
+    nodes: nodeList[0].nodes,
     user: req.user ? req.user : undefined,
   });
 });
 
 router.post('/registernode', permission(), async function(req, res, next) {
-  const ipPort = `${req.body.ip}:${req.bodyport}`;
-  console.log(ipPort);
+  const ipPort = `${req.body.ip}:${req.body.port}`;
+  const ipArray = [];
+  ipArray.push(ipPort);
+  console.log(ipArray);
+
+  const nodeList = await db('users')
+  .select('nodes')
+  .from('users')
+  .where('id', req.user.id)
+  .limit(1);
+
+  const oldNodeArray = JSON.parse(nodeList[0].nodes);
+  console.log(oldNodeArray);
+
+  const combinedArray = oldNodeArray.concat(ipArray);
+  console.log(combinedArray);
+
+
+
   await db('users')
     .update({
-      nodes: ipPort,
+      nodes: JSON.stringify(combinedArray),
     })
     .where('id', req.user.id)
     .limit(1);
